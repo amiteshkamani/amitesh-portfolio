7be72ac24d6691649455f21b746c14ca4f835b97
Varun Goel <varungoel0806@gmail.com>
2025-04-22 20:42:38 +0200

    Changed root of default page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ import Home from "@/pages/Home";
 function Router() {
   return (
     <Switch>
-      <Route path="/" component={Home} />
+      <Route path="/amitesh-portfolio" component={Home} />
       <Route component={NotFound} />
     </Switch>
   );
